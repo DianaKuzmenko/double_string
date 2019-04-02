3d4710885b2714ca9c7b88e90afc870664481244
DianaKuzmenko <diana9142@gmail.com>
2019-04-02 16:28:38 +0300

    Update double_string.py

--- double_string.py
+++ double_string.py
@@ -1,6 +1,7 @@
-def double_str('aa'):
+def double_str(s):
     s1 = '*' + s
     while len(s1) < (2*len(s)+1):
         s1 = s1.replace('*a','aa*')
     s1 = s1.replace('*','')
     print(s1)
+double_str('aaa')
