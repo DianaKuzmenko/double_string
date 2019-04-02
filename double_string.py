ddd522d05157ad36b30e09a152592f197922b719
DianaKuzmenko <diana9142@gmail.com>
2019-04-02 18:05:55 +0300

    Update program
    Let's make a better understandable output with '-->'

--- double_string.py
+++ double_string.py
@@ -1,9 +1,9 @@
 def double_str(s):
     s1 = '*' + s
-    result = s1
+    result = s1 + ' --> '
     while len(s1) < (2*len(s)+1):
         s1 = s1.replace('*a','aa*')
-        result += s1
+        result += s1 + ' --> '
     s1 = s1.replace('*','')
-    print(result)
+    print(result+s1)
 double_str('aaa')
